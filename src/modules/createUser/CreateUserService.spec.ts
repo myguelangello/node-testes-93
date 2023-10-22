2820542f8a76d5a63192171c674402fc255f1ec0
myguelangello <myguelangello624@gmail.com>
2023-10-22 18:59:01 -0300

    test(CreateUser): add tests in services

--- src/modules/createUser/CreateUserService.spec.ts
+++ src/modules/createUser/CreateUserService.spec.ts
@@ -1,37 +1,44 @@
 import { User } from '../../entities/User'
-import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory'
 import { IUsersRepository } from '../../repositories/IUsersRepositories'
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory'
 import { CreateUserService } from './CreateUserService'
 
-describe('Create user', () => {
+describe('Create User', () => {
   let usersRepository: IUsersRepository
   let createUserService: CreateUserService
 
   beforeAll(() => {
+    // chamando o repositorio necessário para o service
     usersRepository = new UsersRepositoryInMemory()
+    // chamando o execute do service
     createUserService = new CreateUserService(usersRepository)
   })
 
   it('should be able to create a new user', async () => {
     const userData: User = {
-      name: 'Test Name',
-      email: 'test@test.com.br',
-      username: 'testusername',
+      email: 'user@example.com',
+      username: 'john.doe',
+      name: 'John Doe',
     }
 
     const user = await createUserService.execute(userData)
 
     expect(user).toHaveProperty('id')
-    expect(user.username).toBe('testusername')
+    expect(user.email).toBe('user@example.com')
   })
 
   it('should not be able to create an existing user', async () => {
     const userData: User = {
-      name: 'Test Existing Name',
-      email: 'testexisting@test.com.br',
-      username: 'testexisting',
+      email: 'user-existing@example.com',
+      username: 'john.existing',
+      name: 'John Doe',
     }
 
+    /**
+     * para verificar se o usuário já existe eu vou criálo uma vez
+     * e depois experar que ao criar um usuário com as mesmas informações
+     * ele rejeite e gere um erro exatamente como está no service.
+     */
     await createUserService.execute(userData)
 
     await expect(createUserService.execute(userData)).rejects.toEqual(
